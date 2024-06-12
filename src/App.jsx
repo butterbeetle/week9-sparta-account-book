8e45e5ecb2a9fa55bfc5a69e7920137ebb7af38b
butterbeetle <butterbeetle@naver.com>
2024-06-12 20:25:47 +0900

    Comment: TODO 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,6 @@ import router from "./routes/router";
 // const persistor = persistStore(store);
 
 //TODO 프로필 이미지 처리
-//TODO accessToken 로그아웃...?
-//TODO 로그인 정보 zustand...?
 export default function App() {
   return (
     <QueryProvider>
