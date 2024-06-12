09d1deffc32a2e1244f0b411207a3445890cf083
butterbeetle <butterbeetle@naver.com>
2024-06-12 14:45:36 +0900

    Comment: 불필요한 코드 주석처리 및 TODO 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { RouterProvider } from "react-router-dom";
-import { persistStore } from "redux-persist";
 import ToastContextProvider from "./context/toast.context";
 import QueryProvider from "./query/QueryProvider";
-import store from "./redux/store/store";
 import router from "./routes/router";
 
-const persistor = persistStore(store);
+// const persistor = persistStore(store);
+
+//TODO 프로필 이미지 처리
 //TODO accessToken 로그아웃...?
 //TODO 로그인 정보 zustand...?
 export default function App() {
