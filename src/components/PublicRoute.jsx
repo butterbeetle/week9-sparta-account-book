7a07e50ab529313ee91484c763f495c62b517935
butterbeetle <butterbeetle@naver.com>
2024-06-12 17:51:51 +0900

    Refactor: SignUp Page Refactoring

--- src/components/PublicRoute.jsx
+++ src/components/PublicRoute.jsx
@@ -3,9 +3,10 @@ import useMe from "../hooks/useMe";
 
 export default function PublicRoute() {
   const { isLoggedIn } = useMe();
+  const token = localStorage.getItem("token");
   // console.log("PROTECTED ROUTE LOGGEDIN___", isLoggedIn);
 
-  if (isLoggedIn) {
+  if (token || isLoggedIn) {
     return <Navigate to="/" replace />;
   }
 
--- src/pages/SignUpPage/SignUpPage.jsx
+++ src/pages/SignUpPage/SignUpPage.jsx
@@ -1,48 +1,33 @@
-import { useState } from "react";
+import { useId, useRef } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
-import DataInput from "../../components/DataInput";
 import { useToast } from "../../context/toast.context";
 import useMe from "../../hooks/useMe";
-
-const signUpDatas = [
-  { id: "id", label: "아이디", minLength: 4, maxLength: 10 },
-  {
-    id: "password",
-    type: "password",
-    label: "비밀번호",
-    minLength: 4,
-    maxLength: 15,
-  },
-  {
-    id: "nickname",
-    label: "닉네임",
-    minLength: 1,
-    maxLength: 10,
-  },
-];
 
 function SignUpPage() {
   const toast = useToast();
   const nav = useNavigate();
   const { signUp } = useMe();
 
-  const [inputData, setInputData] = useState({});
+  const inputRef = useRef([]);
+  const userId = useId();
+  const passwordId = useId();
+  const nickNameId = useId();
 
-  /**
-   * 아이디맨
-   * 123123
-   * 니크네크임
-   */
   const onSubmitHandler = async (e) => {
     e.preventDefault();
-    // console.log("SIGN-UP SUBMIT___");
-    // console.log(inputData);
+
+    const signUpUserInfo = {
+      id: inputRef.current[0].value,
+      password: inputRef.current[1].value,
+      nickname: inputRef.current[2].value,
+    };
+
     try {
-      // console.log("SUCCESS___");
+      // console.log("SIGN UP SUCCESS___");
       const {
         data: { message },
-      } = await signUp(inputData);
+      } = await signUp(signUpUserInfo);
 
       toast.createToast({
         id: uuidv4(),
@@ -54,7 +39,7 @@ function SignUpPage() {
       nav("/login", { replace: true });
     } catch (error) {
       const { code, message, response } = error;
-      console.log("SIGN UP ERROR___", code, message, response.data.message);
+      // console.log("SIGN UP ERROR___", code, message, response.data.message);
       toast.createToast({
         id: uuidv4(),
         title: code,
@@ -75,18 +60,82 @@ function SignUpPage() {
         onSubmit={(e) => onSubmitHandler(e)}
         className="flex flex-col gap-3 w-full"
       >
-        {signUpDatas.map(({ id, type, label, minLength, maxLength }) => (
-          <DataInput
-            key={id}
-            id={id}
-            type={type}
-            label={label}
-            inputData={inputData[id]}
-            setInputData={setInputData}
-            minLength={minLength}
-            maxLength={maxLength}
+        <div className="relative">
+          <input
+            ref={(el) => (inputRef.current[0] = el)}
+            className="p-6 pb-px w-full text-base appearance-none outline-none
+                     border border-solid border-[#0a0426] rounded-md text-[#0a0426]
+                     hover:shadow-md peer select-none"
+            id={userId}
+            defaultValue={""}
+            placeholder=""
+            minLength={4}
+            maxLength={10}
           />
-        ))}
+          <label
+            className="absolute top-4 left-6 text-base select-none text-[#a1a1aa] cursor-text
+                     duration-150 transform
+                     origin-[0]
+                     -translate-y-3 scale-75
+                     peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0
+                     peer-focus:scale-75 peer-focus:-translate-y-3"
+            htmlFor={userId}
+          >
+            <div className="flex">{"아이디"}</div>
+          </label>
+        </div>
+
+        <div className="relative">
+          <input
+            ref={(el) => (inputRef.current[1] = el)}
+            className="p-6 pb-px w-full text-base appearance-none outline-none
+                     border border-solid border-[#0a0426] rounded-md text-[#0a0426]
+                     hover:shadow-md peer select-none"
+            id={passwordId}
+            defaultValue={""}
+            placeholder=""
+            type="password"
+            minLength={4}
+            maxLength={15}
+          />
+          <label
+            className="absolute top-4 left-6 text-base select-none text-[#a1a1aa] cursor-text
+                     duration-150 transform
+                     origin-[0]
+                     -translate-y-3 scale-75
+                     peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0
+                     peer-focus:scale-75 peer-focus:-translate-y-3"
+            htmlFor={passwordId}
+          >
+            <div className="flex">{"비밀번호"}</div>
+          </label>
+        </div>
+
+        <div className="relative">
+          <input
+            ref={(el) => (inputRef.current[2] = el)}
+            className="p-6 pb-px w-full text-base appearance-none outline-none
+                     border border-solid border-[#0a0426] rounded-md text-[#0a0426]
+                     hover:shadow-md peer select-none"
+            id={nickNameId}
+            defaultValue={""}
+            placeholder=""
+            minLength={1}
+            maxLength={10}
+          />
+          <label
+            className="absolute top-4 left-6 text-base select-none text-[#a1a1aa] cursor-text
+                     duration-150 transform
+                     origin-[0]
+                     -translate-y-3 scale-75
+                     peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0
+                     peer-focus:scale-75 peer-focus:-translate-y-3"
+            htmlFor={nickNameId}
+          >
+            <div className="flex">{"닉네임"}</div>
+          </label>
+        </div>
+
         <button
           className="p-3 text-base font-bold text-white bg-[#0a0426] border-none rounded-lg cursor-pointer
       hover:bg-[#1c1c3b] hover:shadow-md active:bg-[#2c2c3b] active:shadow-inner"
