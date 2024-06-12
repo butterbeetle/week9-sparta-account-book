381b5ff87a97581f0f3bd2dd59562dba1db7854f
butterbeetle <butterbeetle@naver.com>
2024-06-12 11:12:54 +0900

    Refactor: MyPage useState -> useRef - 실시간으로 데이터 추적할 필요 없으므로

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -1,9 +1,8 @@
 import { useMutation } from "@tanstack/react-query";
-import { useState } from "react";
+import { useId, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useShallow } from "zustand/react/shallow";
 import api from "../../api/api";
-import DataInput from "../../components/DataInput";
 import useLoginStore from "../../zustand/login.store";
 
 function MyPage() {
@@ -22,7 +21,9 @@ function MyPage() {
   );
 
   const [file, setFile] = useState();
-  const [inputData, setInputData] = useState({ nickname });
+
+  const inputRef = useRef(null);
+  const nickNameId = useId();
 
   const onChangeHandler = (e) => {
     e.preventDefault();
@@ -36,7 +37,7 @@ function MyPage() {
     const token = JSON.parse(localStorage.getItem("token"));
     await updateUserInfo({
       token,
-      data: { avatar: file, ...inputData },
+      data: { avatar: file, nickname: inputRef.current.value },
     });
     nav("/");
   };
@@ -54,14 +55,36 @@ function MyPage() {
         ></img>
       </div>
       <input type="file" onChange={(e) => onChangeHandler(e)} />
-      <DataInput
-        id={"nickname"}
-        label={"닉네임"}
-        inputData={inputData["nickname"]}
-        setInputData={setInputData}
-        minLength={1}
-        maxLength={10}
-      />
+      <div className="relative">
+        <input
+          ref={(el) => (inputRef.current = el)}
+          className="p-6 pb-px w-full text-base appearance-none outline-none
+                     border border-solid border-[#0a0426] rounded-md text-[#0a0426]
+                     hover:shadow-md peer select-none"
+          id={nickNameId}
+          defaultValue={nickname}
+          minLength={1}
+          maxLength={10}
+        />
+        <label
+          className="absolute top-4 left-6 text-base select-none text-[#a1a1aa] cursor-text
+                     duration-150 transform
+                     origin-[0]
+                     -translate-y-3 scale-75
+                     peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0
+                     peer-focus:scale-75 peer-focus:-translate-y-3"
+          htmlFor={nickNameId}
+        >
+          <div className="flex">
+            {"닉네임"}
+            {/* {errorData[id] && (
+            <p className="cursor-text text-base text-red-700 select-none ">{`${
+              type === "date" ? "를" : "을"
+            } 제대로 입력해주세요.`}</p>
+          )} */}
+          </div>
+        </label>
+      </div>
       <button
         onClick={() => onClickHandler()}
         className="p-3 text-base font-bold text-white bg-[#0a0426] border-none rounded-lg cursor-pointer
